f9dec7038a86f76fc8e30801c2eccce9f2d18c4c
gdm1995 <gdm2009@hotmail.it>
2017-12-15 01:01:04 +0100

    Fixed Imports

--- ProgettoPW2017/src/Controller/Acquistato.java
+++ ProgettoPW2017/src/Controller/Acquistato.java
@@ -1,20 +1,12 @@
 package Controller;
 
 import java.io.IOException;
-import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import Model.User;
-import OperazioniDAO.Controllo_LoginDAO;
-import OperazioniDAO.DeleteProdDAO;
-import OperazioniDAO.InsertAmmDAO;
-import OperazioniDAO.ModificaDAO;
 
 /**
  * Classe di controllo per interagire col database al fine di segnare come acquistato un prodotto. Deselezionandolo dagli acquistati e riducendo la quantità residuea
